b7888545d661fa2502571be40b4205943c8d09ad
tamilprog <tamilkumaran15102004@gmail.com>
2025-06-23 16:52:40 +0530

    Added Files

--- model/Student.java
+++ model/Student.java
@@ -1,5 +1,16 @@
 package model;
 
 public class Student {
+    private int studentId;
+    private int roomNo;
 
+    public Student(int studentId, int roomNo) {
+        this.studentId = studentId;
+        this.roomNo = roomNo;
+    }
+
+    public String getStudent() {
+        return "Student ID: " + studentId + " | Room No: " + roomNo;
+
+    }
 }
